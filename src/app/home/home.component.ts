50239b9dc22af8adcf98ba4978a2e91c0888ba78
Marvin Kweyu <mkweyu1@gmail.com>
2022-10-19 20:04:55 +0300

    chore: display functioning counter

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,53 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
 
-  initial_time: Number = 25;
-  
-  constructor() { }
+  timer = 1800; // set the time at 30 minutes
 
-  ngOnInit(): void {
-   
-  }
+  countDown: any;
+  constructor(private elementRef: ElementRef) {}
 
-  startCounter(initial_time: any) {
-  
-    setInterval(() => this.decreaseTime(initial_time), 1000);
-  }
-  decreaseTime(initial_time: any) {
+  ngOnInit() {}
+
+  startTimer(display: { textContent: string }) {
     
-    console.log("Starting the counter");
-    console.log(initial_time);
-    // this.initial_time - 1;
+    let minutes;
+    let seconds;
+
+    this.countDown = interval(1000).subscribe((x) => {
+      minutes = Math.floor(this.timer / 60);
+      seconds = Math.floor(this.timer % 60);
+
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+
+      display.textContent = minutes + ':' + seconds;
+
+      --this.timer;
+      if (this.timer < 0) {
+        // perform notification of completion
+        console.log('timer is ended');
+      }
+    });
     
-    console.log("delete")
   }
 
 
+  startCounter() {
+    // will disable after click
+    let callDuration = this.elementRef.nativeElement.querySelector('#time');
+    this.startTimer(callDuration);
+  }
+  //  this button is disabled at the start of the program
+  stopCounter(){
+    this.countDown.unsubscribe();
+    console.log("stop coutner")
+    // enable start button counter
+  }
 }
